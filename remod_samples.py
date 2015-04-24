abd27c9de9e617e1ffe44c38083c6c4cafd84d69
Gipson P <pullag@users.noreply.github.com>
2015-04-24 17:53:35 +0530

    create remodule sample

---
+++ remod_samples.py
@@ -0,0 +1,29 @@
+import re
+str = "When did harry met sally"
+
+##match = re.search('harry', str)
+##if match:
+##    print('found')
+##else:
+##    print('dude try again')
+
+
+##p = re.compile('[a-z]{3,6}')
+##if p.match('harry'):
+##    print ("found the match")
+##else:
+##    print("didnt found the compiled match")
+
+##p = re.search('harry', str)
+##if p:
+##    print("found the match")
+##else:
+##    print("the match is not present")
+
+
+##p = re.findall('[a-z]+', str, re.I)
+##print (p)
+
+##p = re.finditer('[a-z]+', str, re.I)
+##for word in p:
+##    print(word.span())
